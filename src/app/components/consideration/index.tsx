inconsiderable/issues

prepare frontier for pass-trail

src/app/components/consideration/index.tsx
@@ -1,0 +1,215 @@
+import {
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  useIonModal,
+  IonText,
+  IonNote,
+  IonContent,
+  IonPage,
+  IonButton,
+  IonToolbar,
+  IonHeader,
+  IonButtons,
+  IonCardSubtitle,
+  IonIcon,
+  useIonActionSheet,
+} from '@ionic/react';
+import timeago from 'epoch-timeago';
+import { Consideration } from '../../utils/appTypes';
+import KeyViewer from '../keyViewer';
+import { useClipboard } from '../../useCases/useClipboard';
+import { ellipsisVertical } from 'ionicons/icons';
+import {
+  considerationID,
+  getConsReference,
+  shortenB64,
+} from '../../utils/compat';
+import { useContext, useEffect } from 'react';
+import { AppContext } from '../../utils/appContext';
+import { OverlayEventDetail } from '@ionic/core/components';
+
+export const ConsiderationItem: React.FC<Consideration> = (consideration) => {
+  const { requestConsideration, getConsiderationByID } = useContext(AppContext);
+
+  const referenceID = getConsReference(consideration);
+  const referenced = getConsiderationByID(referenceID);
+
+  useEffect(() => {
+    if (referenceID && !referenced) {
+      requestConsideration(referenceID);
+    }
+  }, [referenceID, referenced, requestConsideration]);
+
+  const [present, dismiss] = useIonModal(ConsiderationDetail, {
+    onDismiss: () => dismiss(),
+    consideration,
+    referenced,
+  });
+
+  const { time, memo } = consideration;
+
+  const timeMS = time * 1000;
+
+  return (
+    <IonItem lines="none" onClick={() => present()}>
+      <IonLabel className="ion-text-wrap">
+        <IonText color="tertiary">
+          <sub>
+            <time dateTime={new Date(timeMS).toISOString()}>
+              <p>{timeago(timeMS)}</p>
+            </time>
+          </sub>
+        </IonText>
+        {memo && <p>{memo}</p>}
+        {referenced && (
+          <IonCard>
+            <IonCardContent>{referenced.memo}</IonCardContent>
+          </IonCard>
+        )}
+      </IonLabel>
+    </IonItem>
+  );
+};
+
+export default ConsiderationItem;
+
+interface ConsiderationListProps {
+  heading?: string;
+  considerations: Consideration[];
+}
+
+export const ConsiderationList = ({
+  considerations,
+  heading,
+}: ConsiderationListProps) => {
+  return (
+    <IonList>
+      {heading && (
+        <IonListHeader>
+          <IonLabel>{heading}</IonLabel>
+        </IonListHeader>
+      )}
+      {!considerations.length && (
+        <IonItem>
+          <IonLabel>No Activity</IonLabel>
+        </IonItem>
+      )}
+      {considerations.map((tx, index) => (
+        <ConsiderationItem
+          key={index}
+          by={tx.by}
+          for={tx.for}
+          memo={tx.memo}
+          time={tx.time}
+          nonce={tx.nonce}
+          series={tx.series}
+        />
+      ))}
+    </IonList>
+  );
+};
+
+export const ConsiderationDetail = ({
+  onDismiss,
+  consideration,
+  referenced,
+}: {
+  onDismiss: () => void;
+  consideration: Consideration;
+  referenced: Consideration;
+}) => {
+  const { copyToClipboard } = useClipboard();
+
+  const [presentActionSheet] = useIonActionSheet();
+
+  const handleActionSheet = ({ data }: OverlayEventDetail) => {
+    if (data?.['action'] === 'copy') {
+      copyToClipboard(considerationID(consideration));
+    }
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonButton color="medium" onClick={() => onDismiss()}>
+              Close
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}
+            >
+              <div>
+                Considered by:{' '}
+                <KeyViewer
+                  value={
+                    consideration.by ??
+                    '0000000000000000000000000000000000000000000='
+                  }
+                />
+              </div>
+              <IonButton
+                className="ion-no-padding"
+                fill="clear"
+                onClick={() => {
+                  presentActionSheet({
+                    onDidDismiss: ({ detail }) => handleActionSheet(detail),
+                    header: `${shortenB64(
+                      consideration.by ?? '0000000',
+                    )} => ${shortenB64(consideration.for)}`,
+                    buttons: [
+                      {
+                        text: 'Copy Rep ID',
+                        data: {
+                          action: 'copy',
+                        },
+                      },
+                    ],
+                  });
+                }}
+              >
+                <IonIcon
+                  color="primary"
+                  slot="icon-only"
+                  icon={ellipsisVertical}
+                ></IonIcon>
+              </IonButton>
+            </IonCardSubtitle>
+            <IonLabel>
+              <IonNote>
+                {new Date(consideration.time * 1000).toDateString()}
+              </IonNote>
+            </IonLabel>
+          </IonCardHeader>
+          <IonCardContent>
+            <KeyViewer value={consideration.for} />
+            <p>{consideration.memo}</p>
+            {referenced && (
+              <IonCard>
+                <IonCardContent>
+                  <KeyViewer value={referenced.for} />
+                  <p>{referenced.memo}</p>
+                </IonCardContent>
+              </IonCard>
+            )}
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
+  );
+};

src/app/components/mapList/index.tsx
@@ -1,0 +1,70 @@
+import {
+  IonBadge,
+  IonIcon,
+  IonItem,
+  IonItemDivider,
+  IonLabel,
+  IonList,
+} from '@ionic/react';
+import { listOutline, chevronForwardOutline } from 'ionicons/icons';
+import KeyViewer from '../keyViewer';
+
+const MapList = ({
+  connected,
+  selectedKey,
+  setSelectedKey,
+}: {
+  selectedKey: string;
+  connected: {
+    pubkey: string;
+    ranking: number;
+    label: string;
+    catchment?: string;
+  }[];
+  setSelectedKey: (key: string) => void;
+}) => {
+  const forKey = connected.find((k) => k.pubkey === selectedKey);
+  return (
+    <IonList>
+      <IonItem lines="none" unselectable="on">
+        <IonLabel>
+          <KeyViewer value={selectedKey} label={forKey?.label} />
+        </IonLabel>
+        <IonBadge className="ion-margin-start">
+          {Number((forKey?.ranking ?? 0 / 1) * 100).toFixed(2)}%
+        </IonBadge>
+        <IonIcon slot="end" icon={listOutline}></IonIcon>
+      </IonItem>
+      <IonItem>
+        {forKey?.catchment && (
+          <IonBadge className="ion-margin-start">{forKey?.catchment}</IonBadge>
+        )}
+      </IonItem>
+      <IonItemDivider></IonItemDivider>
+      {connected
+        .filter((k) => k.pubkey !== selectedKey)
+        .sort((a, b) => b.ranking - a.ranking)
+        .map(({ ranking, pubkey, label, catchment }) => (
+          <IonItem
+            lines="none"
+            key={pubkey}
+            aria-selected={selectedKey === pubkey}
+            onClick={() => {
+              setSelectedKey(pubkey);
+            }}
+          >
+            <IonLabel>
+              <KeyViewer readonly value={pubkey} label={label} />
+            </IonLabel>
+
+            <IonBadge className="ion-margin-start">
+              {Number((ranking / 1) * 100).toFixed(2)}%
+            </IonBadge>
+            <IonIcon slot="end" icon={chevronForwardOutline}></IonIcon>
+          </IonItem>
+        ))}
+    </IonList>
+  );
+};
+
+export default MapList;

src/app/pages/map.tsx
@@ -1,0 +1,84 @@
+import { PageShell } from '../components/pageShell';
+import { useKeyHolder } from '../useCases/useKeyHolder';
+import { useContext, useEffect, useState } from 'react';
+import { AppContext } from '../utils/appContext';
+import CatchmentMap from '../components/mapGraph';
+import { parseGraphDOT } from '../utils/compat';
+
+const Map = () => {
+  const { selectedKey } = useKeyHolder();
+
+  const {
+    colorScheme,
+    tipHeader,
+    graphDOT,
+    requestGraphDOT,
+    rankingFilter,
+    setRankingFilter,
+  } = useContext(AppContext);
+
+  const tipHeight = tipHeader?.header.height ?? 0;
+
+  const [peekGraphKey, setPeekGraphKey] = useState<string | null | undefined>();
+
+  const whichKey =
+    peekGraphKey ||
+    selectedKey ||
+    '0000000000000000000000000000000000000000000=';
+
+  useEffect(() => {
+    const timeoutId = window.setTimeout(() => {
+      if (whichKey) {
+        requestGraphDOT(whichKey);
+      }
+    }, 0);
+    return () => {
+      window.clearTimeout(timeoutId);
+    };
+  }, [whichKey, requestGraphDOT]);
+
+  useEffect(() => {
+    const resultHandler = (data: any) => {
+      if (whichKey && data.detail) {
+        requestGraphDOT(whichKey);
+      }
+    };
+
+    document.addEventListener('inv_pass', resultHandler);
+
+    return () => {
+      document.removeEventListener('inv_pass', resultHandler);
+    };
+  }, [whichKey, requestGraphDOT]);
+
+  const gdot = graphDOT(whichKey);
+
+  const { nodes, links } = parseGraphDOT(gdot, whichKey, rankingFilter);
+
+  return (
+    <PageShell
+      renderBody={() => (
+        <>
+          {!!whichKey && (
+            <>
+              {!!gdot && (
+                <CatchmentMap
+                  tipHeight={tipHeight}
+                  forKey={whichKey}
+                  nodes={nodes ?? []}
+                  links={links ?? []}
+                  setForKey={setPeekGraphKey}
+                  rankingFilter={rankingFilter}
+                  setRankingFilter={setRankingFilter}
+                  colorScheme={colorScheme}
+                />
+              )}
+            </>
+          )}
+        </>
+      )}
+    />
+  );
+};
+
+export default Map;

src/app/utils/appTypes.ts
@@ -1,0 +1,58 @@
+export interface Profile {
+  public_key: string;
+  ranking: number;
+  imbalance: number;
+  catchment?: string;
+  pass_id?: string;
+  height?: number;
+  error?: string;
+}
+
+export interface GraphNode {
+  id: number;
+  label: string;
+  catchment?: string;
+  catchmentIndex?: number;
+  group?: number;
+  neighbors?: GraphNode[];
+  links?: GraphLink[];
+  pubkey: string;
+  ranking: number;
+}
+
+export interface GraphLink {
+  source: number;
+  target: number;
+  value: number;
+}
+
+export interface PassHeader {
+  previous: string;
+  hash_list_root: string;
+  time: number;
+  target: string;
+  trail_work: string;
+  nonce: number;
+  height: number;
+  consideration_count: number;
+}
+
+export interface PassIdHeaderPair {
+  pass_id: string;
+  header: PassHeader;
+}
+
+export interface Pass {
+  header: PassHeader;
+  considerations: Consideration[];
+}
+
+export interface Consideration {
+  time: number;
+  nonce?: number;
+  by?: string;
+  for: string;
+  memo: string;
+  series?: number;
+  signature?: string;
+}

src/app/utils/compat.ts
@@ -1,0 +1,134 @@
+import { Graph } from 'ngraph.graph';
+import fromDot from 'ngraph.fromdot';
+import { sha3_256 } from 'js-sha3';
+import { GraphLink, GraphNode, Consideration } from './appTypes';
+
+export const parseGraphDOT = (
+  dotString: string = 'digraph{}',
+  forKey: string,
+  rankingFilter: number,
+) => {
+  const graph: Graph = fromDot(dotString);
+
+  const nodes: GraphNode[] = [];
+
+  graph.forEachNode((node: any) => {
+    const ranking = Number(node.data.ranking);
+    const pubkey = node.data.pubkey as string;
+    const catchment = node.data.catchment as string;
+    const catchmentIndex = Number(node.data.cIndex);
+    const label = node.data.label as string;
+
+    if (forKey !== pubkey && rankingFilter / 100 > ranking) return;
+
+    nodes.push({
+      id: node.id,
+      group: 1,
+      label,
+      pubkey,
+      ranking,
+      catchment,
+      catchmentIndex,
+    });
+  });
+
+  const links: GraphLink[] = [];
+  graph.forEachLink((link: any) => {
+    const source = link.fromId;
+    const target = link.toId;
+
+    if (!nodes.map((n) => n.id).includes(source)) return;
+    if (!nodes.map((n) => n.id).includes(target)) return;
+
+    links.push({
+      source,
+      target,
+      value: Number(link.data.weight),
+    });
+  });
+  return { nodes, links };
+};
+
+// hex to base64
+// Function to convert Hexadecimal to Base64
+export function hexToBase64(hex: string): string {
+  return btoa(
+    hex
+      .match(/\w{2}/g)!
+      .map((byte) => String.fromCharCode(parseInt(byte, 16)))
+      .join(''),
+  );
+}
+
+// Function to convert Base64 to Hexadecimal
+export function base64ToHex(base64: string): string {
+  const binaryString = atob(base64);
+  let hexResult = '';
+  for (let i = 0; i < binaryString.length; i++) {
+    const hex = binaryString.charCodeAt(i).toString(16);
+    hexResult += hex.length === 2 ? hex : '0' + hex;
+  }
+  return hexResult.toUpperCase();
+}
+
+export const shortenPassID = (value: string) => {
+  const b64 = hexToBase64(value);
+
+  return `${b64.substring(0, 7)}...${b64.substring(37)}`;
+};
+
+export const shortenHex = (value: string) => {
+  return `${value.substring(0, 5)}...${value.substring(60)}`;
+};
+
+// export const catchmentPrefix = (input: string) => {
+//   const split = input.split('/');
+//   const catchmentIndex = Number(split[0]);
+
+//   if (split.length > 1 || isNaN(catchmentIndex)) {
+//     return { isValid: false };
+//   }
+
+//   return { isValid: true, catchmentIndex };
+// };
+
+export const shortenB64 = (value: string) => {
+  if (value.startsWith('0000000000000000')) {
+    return value.substring(0, 15);
+  }
+
+  return value.replace(/0+=?$/g, '').substring(0, 15);
+
+  //return isValid ? value.replace(/0+=?$/g, '') : `${value.substring(0, 15)}`; //...${value.substring(40)}`;
+};
+
+export const considerationID = (consideration: Consideration) => {
+  const obj = {
+    //IMPORTANT: The order here must be preserved when stringified for generating consistent hashes
+    time: consideration.time,
+    nonce: consideration.nonce,
+    by: consideration.by,
+    for: consideration.for,
+    memo: consideration.memo,
+    series: consideration.series,
+  };
+
+  const rep_hash = sha3_256(JSON.stringify(obj));
+
+  return rep_hash;
+};
+
+export const getConsReference = ({ memo }: Consideration) => {
+  //"Example reference in memo looks like this: ref(xxxxhexadecimalxxxxxx)";
+
+  const regex = /ref\(([a-fA-F0-9]+)\)/g;
+
+  let match;
+  const hexNumbers = [];
+
+  while ((match = regex.exec(memo)) !== null) {
+    hexNumbers.push(match[1]); // match[1] contains the hexadecimal part
+  }
+
+  return hexNumbers[0] ?? '';
+};

inconsiderable/fix

src/app/components/consideration/index.tsx
@@ -1,0 +1,212 @@
+import {
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  useIonModal,
+  IonText,
+  IonNote,
+  IonContent,
+  IonPage,
+  IonButton,
+  IonToolbar,
+  IonHeader,
+  IonButtons,
+  IonCardSubtitle,
+  IonIcon,
+  useIonActionSheet,
+} from '@ionic/react';
+import timeago from 'epoch-timeago';
+import { Consideration } from '../../utils/appTypes';
+import KeyViewer from '../keyViewer';
+import { useClipboard } from '../../useCases/useClipboard';
+import { ellipsisVertical } from 'ionicons/icons';
+import { considerationID, getReference, shortenB64 } from '../../utils/compat';
+import { useContext, useEffect } from 'react';
+import { AppContext } from '../../utils/appContext';
+import { OverlayEventDetail } from '@ionic/core/components';
+
+export const ConsiderationItem: React.FC<Consideration> = (consideration) => {
+  const { requestConsideration, getConsiderationByID } = useContext(AppContext);
+
+  const referenceID = getReference(consideration);
+  const referenced = getConsiderationByID(referenceID);
+
+  useEffect(() => {
+    if (referenceID && !referenced) {
+      requestConsideration(referenceID);
+    }
+  }, [referenceID, referenced, requestConsideration]);
+
+  const [present, dismiss] = useIonModal(ConsiderationDetail, {
+    onDismiss: () => dismiss(),
+    consideration,
+    referenced,
+  });
+
+  const { time, memo } = consideration;
+
+  const timeMS = time * 1000;
+
+  return (
+    <IonItem lines="none" onClick={() => present()}>
+      <IonLabel className="ion-text-wrap">
+        <IonText color="tertiary">
+          <sub>
+            <time dateTime={new Date(timeMS).toISOString()}>
+              <p>{timeago(timeMS)}</p>
+            </time>
+          </sub>
+        </IonText>
+        {referenced ? (
+          <IonCard>
+            <IonCardContent>{referenced.memo}</IonCardContent>
+          </IonCard>
+        ) : (
+          <p>{memo}</p>
+        )}
+      </IonLabel>
+    </IonItem>
+  );
+};
+
+export default ConsiderationItem;
+
+interface ConsiderationListProps {
+  heading?: string;
+  considerations: Consideration[];
+}
+
+export const ConsiderationList = ({
+  considerations,
+  heading,
+}: ConsiderationListProps) => {
+  return (
+    <IonList>
+      {heading && (
+        <IonListHeader>
+          <IonLabel>{heading}</IonLabel>
+        </IonListHeader>
+      )}
+      {!considerations.length && (
+        <IonItem>
+          <IonLabel>No Activity</IonLabel>
+        </IonItem>
+      )}
+      {considerations.map((tx, index) => (
+        <ConsiderationItem
+          key={index}
+          by={tx.by}
+          for={tx.for}
+          memo={tx.memo}
+          time={tx.time}
+          nonce={tx.nonce}
+          series={tx.series}
+        />
+      ))}
+    </IonList>
+  );
+};
+
+export const ConsiderationDetail = ({
+  onDismiss,
+  consideration,
+  referenced,
+}: {
+  onDismiss: () => void;
+  consideration: Consideration;
+  referenced: Consideration;
+}) => {
+  const { copyToClipboard } = useClipboard();
+
+  const [presentActionSheet] = useIonActionSheet();
+
+  const handleActionSheet = ({ data }: OverlayEventDetail) => {
+    if (data?.['action'] === 'copy') {
+      copyToClipboard(considerationID(consideration));
+    }
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonButton color="medium" onClick={() => onDismiss()}>
+              Close
+            </IonButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+              }}
+            >
+              <div>
+                Considered by:{' '}
+                <KeyViewer
+                  value={
+                    consideration.by ??
+                    '0000000000000000000000000000000000000000000='
+                  }
+                />
+              </div>
+              <IonButton
+                className="ion-no-padding"
+                fill="clear"
+                onClick={() => {
+                  presentActionSheet({
+                    onDidDismiss: ({ detail }) => handleActionSheet(detail),
+                    header: `${shortenB64(
+                      consideration.by ?? '0000000',
+                    )} => ${shortenB64(consideration.for)}`,
+                    buttons: [
+                      {
+                        text: 'Copy Rep ID',
+                        data: {
+                          action: 'copy',
+                        },
+                      },
+                    ],
+                  });
+                }}
+              >
+                <IonIcon
+                  color="primary"
+                  slot="icon-only"
+                  icon={ellipsisVertical}
+                ></IonIcon>
+              </IonButton>
+            </IonCardSubtitle>
+            <IonLabel>
+              <IonNote>
+                {new Date(consideration.time * 1000).toDateString()}
+              </IonNote>
+            </IonLabel>
+          </IonCardHeader>
+          <IonCardContent>
+            <KeyViewer value={consideration.for} />
+            <p>{consideration.memo}</p>
+            {referenced && (
+              <IonCard>
+                <IonCardContent>
+                  <KeyViewer value={referenced.for} />
+                  <p>{referenced.memo}</p>
+                </IonCardContent>
+              </IonCard>
+            )}
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
+  );
+};

src/app/components/mapList/index.tsx
@@ -1,0 +1,70 @@
+import {
+  IonBadge,
+  IonIcon,
+  IonItem,
+  IonItemDivider,
+  IonLabel,
+  IonList,
+} from '@ionic/react';
+import { listOutline, chevronForwardOutline } from 'ionicons/icons';
+import KeyViewer from '../keyViewer';
+
+const MapList = ({
+  connected,
+  selectedKey,
+  setSelectedKey,
+}: {
+  selectedKey: string;
+  connected: {
+    pubkey: string;
+    ranking: number;
+    label: string;
+    focale?: string;
+  }[];
+  setSelectedKey: (key: string) => void;
+}) => {
+  const forKey = connected.find((k) => k.pubkey === selectedKey);
+  return (
+    <IonList>
+      <IonItem lines="none" unselectable="on">
+        <IonLabel>
+          <KeyViewer value={selectedKey} label={forKey?.label} />
+        </IonLabel>
+        <IonBadge className="ion-margin-start">
+          {Number((forKey?.ranking ?? 0 / 1) * 100).toFixed(2)}%
+        </IonBadge>
+        <IonIcon slot="end" icon={listOutline}></IonIcon>
+      </IonItem>
+      <IonItem>
+        {forKey?.focale && (
+          <IonBadge className="ion-margin-start">{forKey?.focale}</IonBadge>
+        )}
+      </IonItem>
+      <IonItemDivider></IonItemDivider>
+      {connected
+        .filter((k) => k.pubkey !== selectedKey)
+        .sort((a, b) => b.ranking - a.ranking)
+        .map(({ ranking, pubkey, label, focale }) => (
+          <IonItem
+            lines="none"
+            key={pubkey}
+            aria-selected={selectedKey === pubkey}
+            onClick={() => {
+              setSelectedKey(pubkey);
+            }}
+          >
+            <IonLabel>
+              <KeyViewer readonly value={pubkey} label={label} />
+            </IonLabel>
+
+            <IonBadge className="ion-margin-start">
+              {Number((ranking / 1) * 100).toFixed(2)}%
+            </IonBadge>
+            <IonIcon slot="end" icon={chevronForwardOutline}></IonIcon>
+          </IonItem>
+        ))}
+    </IonList>
+  );
+};
+
+export default MapList;

src/app/pages/map.tsx
@@ -1,0 +1,84 @@
+import { PageShell } from '../components/pageShell';
+import { useHeart } from '../useCases/useHeart';
+import { useContext, useEffect, useState } from 'react';
+import { AppContext } from '../utils/appContext';
+import FocalMap from '../components/mapGraph';
+import { parseGraphDOT } from '../utils/compat';
+
+const Map = () => {
+  const { selectedKey } = useHeart();
+
+  const {
+    colorScheme,
+    tipHeader,
+    graphDOT,
+    requestGraphDOT,
+    rankingFilter,
+    setRankingFilter,
+  } = useContext(AppContext);
+
+  const tipHeight = tipHeader?.header.height ?? 0;
+
+  const [peekGraphKey, setPeekGraphKey] = useState<string | null | undefined>();
+
+  const whichKey =
+    peekGraphKey ||
+    selectedKey ||
+    '0000000000000000000000000000000000000000000=';
+
+  useEffect(() => {
+    const timeoutId = window.setTimeout(() => {
+      if (whichKey) {
+        requestGraphDOT(whichKey);
+      }
+    }, 0);
+    return () => {
+      window.clearTimeout(timeoutId);
+    };
+  }, [whichKey, requestGraphDOT]);
+
+  useEffect(() => {
+    const resultHandler = (data: any) => {
+      if (whichKey && data.detail) {
+        requestGraphDOT(whichKey);
+      }
+    };
+
+    document.addEventListener('inv_pass', resultHandler);
+
+    return () => {
+      document.removeEventListener('inv_pass', resultHandler);
+    };
+  }, [whichKey, requestGraphDOT]);
+
+  const gdot = graphDOT(whichKey);
+
+  const { nodes, links } = parseGraphDOT(gdot, whichKey, rankingFilter);
+
+  return (
+    <PageShell
+      renderBody={() => (
+        <>
+          {!!whichKey && (
+            <>
+              {!!gdot && (
+                <FocalMap
+                  tipHeight={tipHeight}
+                  forKey={whichKey}
+                  nodes={nodes ?? []}
+                  links={links ?? []}
+                  setForKey={setPeekGraphKey}
+                  rankingFilter={rankingFilter}
+                  setRankingFilter={setRankingFilter}
+                  colorScheme={colorScheme}
+                />
+              )}
+            </>
+          )}
+        </>
+      )}
+    />
+  );
+};
+
+export default Map;

src/app/utils/appTypes.ts
@@ -1,0 +1,58 @@
+export interface Profile {
+  public_key: string;
+  ranking: number;
+  imbalance: number;
+  focale?: string;
+  pass_id?: string;
+  height?: number;
+  error?: string;
+}
+
+export interface GraphNode {
+  id: number;
+  label: string;
+  focale?: string;
+  focaleIndex?: number;
+  group?: number;
+  neighbors?: GraphNode[];
+  links?: GraphLink[];
+  pubkey: string;
+  ranking: number;
+}
+
+export interface GraphLink {
+  source: number;
+  target: number;
+  value: number;
+}
+
+export interface PassHeader {
+  previous: string;
+  hash_list_root: string;
+  time: number;
+  target: string;
+  trail_work: string;
+  nonce: number;
+  height: number;
+  consideration_count: number;
+}
+
+export interface PassIdHeaderPair {
+  pass_id: string;
+  header: PassHeader;
+}
+
+export interface Pass {
+  header: PassHeader;
+  considerations: Consideration[];
+}
+
+export interface Consideration {
+  time: number;
+  nonce?: number;
+  by?: string;
+  for: string;
+  memo: string;
+  series?: number;
+  signature?: string;
+}

src/app/utils/compat.ts
@@ -1,0 +1,138 @@
+import { Graph } from 'ngraph.graph';
+import fromDot from 'ngraph.fromdot';
+import { sha3_256 } from 'js-sha3';
+import { GraphLink, GraphNode, Consideration } from './appTypes';
+
+export const parseGraphDOT = (
+  dotString: string = 'digraph{}',
+  forKey: string,
+  rankingFilter: number,
+) => {
+  const graph: Graph = fromDot(dotString);
+
+  const nodes: GraphNode[] = [];
+
+  graph.forEachNode((node: any) => {
+    const ranking = Number(node.data.ranking);
+    const pubkey = node.data.pubkey as string;
+    const focale = node.data.focale as string;
+    const focaleIndex = Number(node.data.fIndex);
+    const label = node.data.label as string;
+
+    if (forKey !== pubkey && rankingFilter / 100 > ranking) return;
+
+    nodes.push({
+      id: node.id,
+      group: 1,
+      label,
+      pubkey,
+      ranking,
+      focale,
+      focaleIndex,
+    });
+  });
+
+  const links: GraphLink[] = [];
+  graph.forEachLink((link: any) => {
+    const source = link.fromId;
+    const target = link.toId;
+
+    if (!nodes.map((n) => n.id).includes(source)) return;
+    if (!nodes.map((n) => n.id).includes(target)) return;
+
+    links.push({
+      source,
+      target,
+      value: Number(link.data.weight),
+    });
+  });
+  return { nodes, links };
+};
+
+// hex to base64
+// Function to convert Hexadecimal to Base64
+export function hexToBase64(hex: string): string {
+  return btoa(
+    hex
+      .match(/\w{2}/g)!
+      .map((byte) => String.fromCharCode(parseInt(byte, 16)))
+      .join(''),
+  );
+}
+
+// Function to convert Base64 to Hexadecimal
+export function base64ToHex(base64: string): string {
+  const binaryString = atob(base64);
+  let hexResult = '';
+  for (let i = 0; i < binaryString.length; i++) {
+    const hex = binaryString.charCodeAt(i).toString(16);
+    hexResult += hex.length === 2 ? hex : '0' + hex;
+  }
+  return hexResult.toUpperCase();
+}
+
+export const shortenPassID = (value: string) => {
+  const b64 = hexToBase64(value);
+
+  return `${b64.substring(0, 7)}...${b64.substring(37)}`;
+};
+
+export const shortenHex = (value: string) => {
+  return `${value.substring(0, 5)}...${value.substring(60)}`;
+};
+
+// export const focalePrefix = (input: string) => {
+//   const split = input.split('/');
+//   const focaleIndex = Number(split[0]);
+
+//   if (split.length > 1 || isNaN(focaleIndex)) {
+//     return { isValid: false };
+//   }
+
+//   return { isValid: true, focaleIndex };
+// };
+
+export const shortenB64 = (value: string) => {
+  if (value.startsWith('0000000000000000000000000000000000000000000')) {
+    return value.substring(0, 1);
+  }
+
+  return value.replace(/0+=?$/g, '').substring(0, 15);
+
+  //return isValid ? value.replace(/0+=?$/g, '') : `${value.substring(0, 15)}`; //...${value.substring(40)}`;
+};
+
+export const isRandomPK = (value: string) => {
+  return !value.endsWith('00=');
+};
+
+export const considerationID = (consideration: Consideration) => {
+  const obj = {
+    //IMPORTANT: The order here must be preserved when stringified for generating consistent hashes
+    time: consideration.time,
+    nonce: consideration.nonce,
+    by: consideration.by,
+    for: consideration.for,
+    memo: consideration.memo,
+    series: consideration.series,
+  };
+
+  const rep_hash = sha3_256(JSON.stringify(obj));
+
+  return rep_hash;
+};
+
+export const getReference = ({ memo }: Consideration) => {
+  //"Example reference in memo looks like this: ref(xxxxhexadecimalxxxxxx)";
+
+  const regex = /ref\(([a-fA-F0-9]+)\)/g;
+
+  let match;
+  const hexNumbers = [];
+
+  while ((match = regex.exec(memo)) !== null) {
+    hexNumbers.push(match[1]); // match[1] contains the hexadecimal part
+  }
+
+  return hexNumbers[0] ?? '';
+};